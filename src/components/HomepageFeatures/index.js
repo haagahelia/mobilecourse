2132d5996e8a5b103f406e1b4cc7a25f2905d68e
juhahinkula <hinkula@gmail.com>
2024-02-28 11:14:46 +0200

    Front page modifications

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,30 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Welcome',
+    Svg: require('@site/static/img/React-icon.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Welcome to Mobile Programming course. This course is based on React Native library. Learning by doing is at the heart of this course.
       </>
     ),
-  },
+   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Schedule & Assignments',
+    Svg: require('@site/static/img/assignments.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        This site contains the course material. The course schedule and assignments can be found from the course Moodle site.
       </>
     ),
-  },
+ },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Course Project',
+    Svg: require('@site/static/img/project.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        The final part of this course is the mobile programming project where you implement your own mobile app. 
+        In this project, you will utilize all the skills that you will learn in this course.
       </>
     ),
   },
