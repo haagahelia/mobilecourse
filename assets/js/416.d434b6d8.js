b26d6fc39d0db571ee6ee7409dfee530fb483cda
juhahinkula <hinkula@gmail.com>
2026-03-06 09:01:04 +0000

    deploy: de28a85bb535a793f8f99c756e1c1641639d8d6f

--- assets/js/416.d434b6d8.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkmobilecourse=globalThis.webpackChunkmobilecourse||[]).push([[416],{416:(e,s,b)=>{b.r(s)}}]);
--- assets/js/8913.c43f48ec.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkmobilecourse=globalThis.webpackChunkmobilecourse||[]).push([[8913],{8913:(e,s,b)=>{b.r(s)}}]);
