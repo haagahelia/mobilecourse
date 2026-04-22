ee9e428d67978fd03c7e8e5fdb98aa3d66fd8d70
juhahinkula <hinkula@gmail.com>
2026-04-22 10:14:53 +0300

    deprecated setting removed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,8 +24,7 @@ const config = {
   projectName: 'mobilecourse', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-
+  
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
